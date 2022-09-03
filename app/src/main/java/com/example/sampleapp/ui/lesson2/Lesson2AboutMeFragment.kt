dd92ca8f91fe4ebcbfa6862d143b1ded7002e46e
Caste11a <zakki0512@gmail.com>
2022-09-03 13:53:54 +0900

    add updateEditNickname

--- app/src/main/java/com/example/sampleapp/ui/lesson2/Lesson2AboutMeFragment.kt
+++ app/src/main/java/com/example/sampleapp/ui/lesson2/Lesson2AboutMeFragment.kt
@@ -7,10 +7,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.core.content.ContextCompat.getSystemService
 import com.example.sampleapp.R
 import com.example.sampleapp.databinding.FragmentLesson2AboutmeBinding
 
@@ -42,6 +44,11 @@ class Lesson2AboutMeFragment : Fragment() {
         view.findViewById<Button>(R.id.done_button).setOnClickListener{
             addNickName(view)
         }
+
+        // ニックネームの編集用
+        view.findViewById<TextView>(R.id.nickname_text).setOnClickListener {
+            updateNickname(view)
+        }
     }
 
     override fun onDestroyView() {
@@ -54,16 +61,37 @@ class Lesson2AboutMeFragment : Fragment() {
     {
         val editText = view.findViewById<EditText>(R.id.nickname_edit)
         val nicknameTextView = view.findViewById<TextView>(R.id.nickname_text)
+        val doneButton = view.findViewById<Button>(R.id.done_button)
 
         nicknameTextView.text = editText.text
         // 編集テキスト、ボタンを非表示にする
         editText.visibility = View.GONE
-//        view.visibility = View.GONE
-
+        doneButton.visibility = View.GONE
+        // ニックネームテキストを表示する
         nicknameTextView.visibility = View.VISIBLE
 
         // キーボードを非表示にする
-//        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-//        imm.hideSoftInputFromWindow(view.windowToken, 0)
+        val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    // update editText
+    private fun updateNickname(view: View) {
+        val editText = view.findViewById<EditText>(R.id.nickname_edit)
+        val nicknameTextView = view.findViewById<TextView>(R.id.nickname_text)
+        val doneButton = view.findViewById<Button>(R.id.done_button)
+
+        // 編集テキストを表示
+        editText.visibility = View.VISIBLE
+        doneButton.visibility = View.VISIBLE
+        // ニックネームテキスト・登録ボタンを非表示
+        nicknameTextView.visibility = View.GONE
+
+        editText.requestFocus()
+
+        // キーボードの表示
+        val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.showSoftInput(editText, 0)
+
     }
 }
